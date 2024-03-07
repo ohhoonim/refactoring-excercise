f86d698fc202c5dfebe238ad2901e629cadb1b11
ohhoonim <hyoungdon.ju@gmail.com>
2024-03-08 07:03:16 +0900

    적립 포인트 로직을 Rental.etFrequentRenterPoints 메서드로 추출 (#7)

--- app/src/main/java/refactoring/excercise/shop/Customer.java
+++ app/src/main/java/refactoring/excercise/shop/Customer.java
@@ -24,15 +24,9 @@ public class Customer {
         String result = getName() + "님의 대여기록\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            // 적립 포인트를 1 포인트 증가
-            frequentRenterPoints++;
-            // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
+            frequentRenterPoints += each.getFrequentRenterPoints();
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-
             // 현재까지 누적된 총 대여료
             totalAmount += each.getCharge();
         }
