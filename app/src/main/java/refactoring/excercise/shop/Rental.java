d9206edbc230f37bea158954a08dedc05e6ce850
주형돈 <hdju@rntime.com>
2024-03-08 06:46:04 +0900

    계산로직을 Rental class로 이동

--- app/src/main/java/refactoring/excercise/shop/Rental.java
+++ app/src/main/java/refactoring/excercise/shop/Rental.java
@@ -16,4 +16,24 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+    
+    public double getCharge(Rental aRental) {
+        double result = 0;
+        switch(aRental.getMovie().getPriceCode()){
+            case Movie.REGULAR:
+                result += 2;
+                if (aRental.getDaysRented() > 2)
+                    result += (aRental.getDaysRented() - 2) * 1.5;  // 대여료 계산
+                break;
+            case Movie.NEW_RELEASE:
+                result += aRental.getDaysRented() * 3;  // 대여료 계산
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (aRental.getDaysRented() > 3)
+                    result += (aRental.getDaysRented() - 3) * 1.5;  // 대여료 계산
+                break;
+        }
+        return result;
+    }
 }
