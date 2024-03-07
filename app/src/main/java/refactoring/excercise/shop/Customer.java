d4f63bb50f2763c554f5ea614c9e036814c8ea1e
주형돈 <hdju@rntime.com>
2024-03-08 06:55:33 +0900

    불필요한 중복 제거 thisAmount

--- app/src/main/java/refactoring/excercise/shop/Customer.java
+++ app/src/main/java/refactoring/excercise/shop/Customer.java
@@ -23,11 +23,7 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = getName() + "님의 대여기록\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            thisAmount = each.getCharge();
-
             // 적립 포인트를 1 포인트 증가
             frequentRenterPoints++;
             // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
@@ -35,10 +31,10 @@ public class Customer {
                 frequentRenterPoints++;
 
             // 이번에 대여하는 비디오 정보와 대여료를 출력
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
 
             // 현재까지 누적된 총 대여료
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
         // 푸터 행 추가
         result += "총 대여료: " + String.valueOf(totalAmount) + "\n";
