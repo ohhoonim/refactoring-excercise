0cf40c5f47fa7a7216aebe9783d980f681d3f8c8
주형돈 <hdju@rntime.com>
2024-03-08 19:55:01 +0900

    frequentRenterPoints 추출

--- app/src/main/java/refactoring/excercise/shop/Customer.java
+++ app/src/main/java/refactoring/excercise/shop/Customer.java
@@ -18,18 +18,26 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = getName() + "님의 대여기록\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
         }
         // 푸터 행 추가
         result += "총 대여료: " + String.valueOf(getTotalCharge()) + "\n";
-        result += "적립 포인트: " + String.valueOf(frequentRenterPoints) + "\n";
+        result += "적립 포인트: " + String.valueOf(getTotalFrequentRenterPoints()) + "\n";
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
         return result;
     }
 
@@ -42,6 +50,7 @@ public class Customer {
         }
         return result; 
     }
+
 
     
 }
