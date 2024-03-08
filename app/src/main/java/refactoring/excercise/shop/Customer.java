bcdf8d550e84ef61cd76a790102a2a5f2d55b1fa
주형돈 <hdju@rntime.com>
2024-03-08 20:05:26 +0900

    htmlStatement 기능 추가

--- app/src/main/java/refactoring/excercise/shop/Customer.java
+++ app/src/main/java/refactoring/excercise/shop/Customer.java
@@ -31,6 +31,20 @@ public class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<h1>" + getName() + "님의 대여기록</h1>\n<ol>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // 이번에 대여하는 비디오 정보와 대여료를 출력
+            result += "\t<li>" + each.getMovie().getTitle() + " : " + String.valueOf(each.getCharge()) + "</li>\n";
+        }
+        // 푸터 행 추가
+        result += "</ol>\n총 대여료: <em>" + String.valueOf(getTotalCharge()) + "</em><br>\n";
+        result += "적립 포인트: <em>" + String.valueOf(getTotalFrequentRenterPoints()) + "</em><br>\n";
+        return result;
+    }
+
     private int getTotalFrequentRenterPoints() {
         int result = 0;
         Enumeration rentals = _rentals.elements();
