b9b1a393d3022a203bd0232a6c74fcf908de283d
주형돈 <hdju@rntime.com>
2024-03-08 19:50:01 +0900

    totalAmount 추출

--- app/src/main/java/refactoring/excercise/shop/Customer.java
+++ app/src/main/java/refactoring/excercise/shop/Customer.java
@@ -18,7 +18,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = getName() + "님의 대여기록\n";
@@ -27,13 +26,21 @@ public class Customer {
             frequentRenterPoints += each.getFrequentRenterPoints();
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            // 현재까지 누적된 총 대여료
-            totalAmount += each.getCharge();
         }
         // 푸터 행 추가
-        result += "총 대여료: " + String.valueOf(totalAmount) + "\n";
+        result += "총 대여료: " + String.valueOf(getTotalCharge()) + "\n";
         result += "적립 포인트: " + String.valueOf(frequentRenterPoints) + "\n";
         return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result; 
     }
 
     
