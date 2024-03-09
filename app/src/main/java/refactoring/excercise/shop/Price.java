3209abbccce3cbd728a5d39b52f253d25ecd177c
주형돈 <hdju@rntime.com>
2024-03-09 15:42:11 +0900

    getFrequentRenterPoints 조건문 재정의

--- app/src/main/java/refactoring/excercise/shop/Price.java
+++ app/src/main/java/refactoring/excercise/shop/Price.java
@@ -5,11 +5,6 @@ public abstract class Price {
     abstract public double getCharge(int daysRented) ;
 
     public int getFrequentRenterPoints(int daysRented) {
-        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return 1;
     }
 }
--- app/src/main/java/refactoring/excercise/shop/moviestate/NewReleasePrice.java
+++ app/src/main/java/refactoring/excercise/shop/moviestate/NewReleasePrice.java
@@ -13,4 +13,9 @@ public class NewReleasePrice extends Price {
     public double getCharge(int daysRented) {
         return daysRented * 3;  // 대여료 계산
     }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        return daysRented > 1 ? 2 : 1;
+    }
 }
